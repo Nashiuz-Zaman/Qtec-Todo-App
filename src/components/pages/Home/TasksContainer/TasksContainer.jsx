407d062a56159164441cfe4bafe86667c85029ba
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-11 01:00:18 +0600

    Change TaskContainer styles

--- src/components/pages/Home/TasksContainer/TasksContainer.jsx
+++ src/components/pages/Home/TasksContainer/TasksContainer.jsx
@@ -7,18 +7,19 @@ import { useSelector } from "react-redux";
 
 const TasksContainer = () => {
   const { tasks } = useSelector(store => store.tasks);
+  const { theme } = useSelector(store => store.websiteTheme);
   console.log(tasks);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
-      <div className="space-y-4">
+    <div className="grid grid-cols-1 2md:grid-cols-[1.5fr_1fr]">
+      <div className="space-y-6">
         {/* if there is no data then show no data component */}
         {tasks?.length < 1 && <NoData text="No todos to show" />}
 
         {/* if there is data show data */}
         {tasks?.length > 0 &&
           tasks.map(task => {
-            return <Task key={task.id} taskData={task} />;
+            return <Task key={task.id} theme={theme} taskData={task} />;
           })}
       </div>
     </div>
