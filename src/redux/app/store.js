1e424b326c3caeb697a72d8521a7c21cd5ce39ec
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-10 20:57:56 +0600

    Create tasks data slice and add it to the store

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -3,9 +3,11 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // reducers
 import websiteThemeReducer from "./../features/websiteTheme/websiteThemeSlice";
+import tasksReducer from "./../features/tasks/tasksSlice";
 
 export const store = configureStore({
   reducer: {
     websiteTheme: websiteThemeReducer,
+    tasks: tasksReducer,
   },
 });
