235e084e337dc20003fc3e91c95d2af6b7a5f8f3
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-10 19:52:54 +0600

    Add theme functionality to App component regarding bg color, Add Header component to RootLayout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,9 +9,17 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Slide } from "react-toastify";
 
+// redux
+import { useSelector } from "react-redux";
+
 const App = () => {
+  const { theme } = useSelector(store => store.websiteTheme);
+
   return (
-    <div className="text-textPrimary font-default min-h-screen flex flex-col max-w-[120rem] mx-auto overflow-x-hidden">
+    <div
+      className={`text-textPrimary font-default min-h-screen flex flex-col max-w-[120rem] mx-auto overflow-x-hidden ${
+        theme === "light" ? "bg-white" : "bg-darkThemeBg"
+      }`}>
       {/* react toastify */}
       <ToastContainer
         position="top-center"
