1c20e34c521dc3f9868f7611a514934f0d7f6f0c
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-11 03:26:48 +0600

    Add TextareaField and CloseBtn to CreateForm

--- src/components/pages/Home/CreateForm/CreateForm.jsx
+++ src/components/pages/Home/CreateForm/CreateForm.jsx
@@ -4,7 +4,10 @@ import PropTypes from "prop-types";
 import InputField from "./../../../shared/InputField/InputField";
 import SelectField from "../../../shared/SelectField/SelectField";
 import ButtonBtn from "./../../../shared/ButtonBtn/ButtonBtn";
+import TextareaField from "../../../shared/TextareaField/TextareaField";
+import CloseBtn from "../../../shared/CloseBtn/CloseBtn";
 
+// select options for the form
 const priorityOptions = [
   {
     id: 0,
@@ -26,14 +29,21 @@ const priorityOptions = [
 const CreateForm = ({ modifyClasses = "" }) => {
   return (
     <form
-      className={`p-customXsm fixed w-[85%] xsm:w-[25rem] md:w-[30rem] lg:w-[35rem] bg-white shadow-large top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-defaultLg ${modifyClasses}`}>
-      <h2 className="font-bold text-2xl mb-4 sm:mb-custom2xsm">Add a Todo</h2>
+      className={`p-customXsm fixed w-[85%] xsm:w-[25rem] md:w-[30rem] lg:w-[35rem] bg-white shadow-large z-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-defaultLg ${modifyClasses}`}>
+      <CloseBtn modifyClasses="my-0 mb-2 text-2xl" />
+
+      <h2 className="font-bold text-2xl mb-5 sm:mb-custom2xsm">Add a Todo</h2>
 
       <div className="space-y-6 mb-8 sm:mb-customXsm">
         <InputField placeholder="Title" name="title" />
-        <InputField placeholder="Description" name="description" />
-        <InputField placeholder="Deadline (DD-MM-YY)" name="deadline" />
+        {/* description */}
+        <TextareaField
+          label="Description"
+          placeholder="Description"
+          name="description"
+        />
 
+        <InputField placeholder="Deadline (DD-MM-YY)" name="deadline" />
         <SelectField label="Priority" options={priorityOptions} />
       </div>
 
