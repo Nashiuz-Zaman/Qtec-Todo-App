acc3848c7e583109ceda2a013e1a0cd401ddedc7
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-11 20:19:37 +0600

    Add tasks filtering functionality in TasksContainer component

--- src/components/pages/Home/TasksContainer/TasksContainer.jsx
+++ src/components/pages/Home/TasksContainer/TasksContainer.jsx
@@ -10,19 +10,23 @@ import useForms from "../../../../hooks/useForms";
 import { useSelector } from "react-redux";
 
 const TasksContainer = () => {
-  const { tasks } = useSelector(store => store.tasks);
+  const { tasks, filter: filterState } = useSelector(store => store.tasks);
   const { theme } = useSelector(store => store.websiteTheme);
   const { openCreateForm } = useForms();
+  const tasksToDisplay =
+    filterState === 0
+      ? tasks
+      : tasks.filter(task => parseInt(task.priorityLevel) === filterState);
 
   return (
     <div className="grid grid-cols-1 2md:grid-cols-[1.5fr_1fr] xl:grid-cols-2 gap-customXsm 2md:gap-0">
       <div className="space-y-6 order-2 2md:order-1">
         {/* if there is no data then show no data component */}
-        {tasks?.length < 1 && <NoData text="No todos to show" />}
+        {tasksToDisplay?.length < 1 && <NoData text="No todos to show" />}
 
         {/* if there is data show data */}
-        {tasks?.length > 0 &&
-          tasks.map(task => {
+        {tasksToDisplay?.length > 0 &&
+          tasksToDisplay.map(task => {
             return <Task key={task.id} theme={theme} taskData={task} />;
           })}
       </div>
